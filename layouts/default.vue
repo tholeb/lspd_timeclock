79bb3893a78c1a47d0dd1e7d77ced7bf5da8fe69
tholeb <43311443+tholeb@users.noreply.github.com>
2023-11-29 23:16:14 +0100

    Add: Default Layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="layout">
+    <p>Some default layout content shared across all pages</p>
+	<el-button @click="ElMessage('hello')">button</el-button>
+	<ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+	<LazyElButton type="warning">lazy button</LazyElButton>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue"
+import autoAnimate from "@formkit/auto-animate"
+
+const layout = ref() // we need a DOM node
+const show = ref(false)
+
+onMounted(() => {
+  autoAnimate(dropdown.value) // thats it!
+})
+</script>
