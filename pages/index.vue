a35902892c3eb494780a634bad370e0de14f3496
tholeb <43311443+tholeb@users.noreply.github.com>
2023-11-29 09:37:44 +0100

    Ref: routing

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const ui = /*ui*/ {
+  background: 'bg-white dark:bg-slate-900'
+}
+</script>
+
+<template>
+	<div>
+		<h1>Nuxt Routing set up successfully!</h1>
+		<p>Current route: {{ route.path }}</p>
+		<a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+	</div>
+	<UCard :ui="ui" />
+</template>
