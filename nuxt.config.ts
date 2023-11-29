96e5d50cb5134385c7e25ced4fda46af47ac3173
tholeb <43311443+tholeb@users.noreply.github.com>
2023-11-29 23:22:42 +0100

    Add: eslint, elementplus, auto-animate, auth configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxt/ui']
+	modules: [
+		'@sidebase/nuxt-auth',
+		'@element-plus/nuxt',
+		'@nuxtjs/eslint-module',
+		'@formkit/auto-animate/nuxt',
+	],
+	auth: {
+		isEnabled: true,
+		baseURL: process.env.AUTH_ORIGIN,
+		provider: {
+			type: 'authjs'
+		},
+		globalAppMiddleware: {
+			isEnabled: true
+		}
+	},
+	elementPlus: {
+		theme: ['dark']
+	},
+	eslint: {}
 })
